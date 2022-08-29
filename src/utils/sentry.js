3c0621e6d01f9cb45f0e0df32b2d56ec07bf91cd
cha0sCat <admin@noob.pw>
2022-08-30 04:07:00 +0900

    feat(sentry): ignoreErrors

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -12,6 +12,15 @@ export default (vue, router) => {
           tracingOrigins: [/.*/],
         }),
       ],
+      ignoreErrors: [
+        'ResizeObserver loop limit exceeded',
+        'ResizeObserver loop completed with undelivered notifications.',
+
+        'AbortError: The play() request was interrupted by a new load request.',
+        'AbortError: The play() request was interrupted by a new load request. https://goo.gl/LdLk22',
+
+        'The fetching process for the media resource was aborted by the user agent at the user\'s request.',
+      ]
       // Set tracesSampleRate to 1.0 to capture 100%
       // of transactions for performance monitoring.
       // We recommend adjusting this value in production
