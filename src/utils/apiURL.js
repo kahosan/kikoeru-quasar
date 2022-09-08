0cfc9f6c155242ed9ced2a1433d99b20bb79e35e
kahosan <15116355657l@gmail.com>
2022-09-08 11:42:31 +0800

    fix: cover url

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -13,25 +13,28 @@ export const coverURL = (metadata, type = 'main') => {
 
   // 应对组件初始化时没有提供 workId 的情况
   if (!(metadata.id || metadata.hash.split('/')[0])) { return '' }
+  console.log(type)
 
   // 优先选择后端返回的 url 地址
-  switch (type) {
-    case 'main':
-      if (metadata.mainCoverUrl) { return metadata.mainCoverUrl }
-      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=main` }
+  // switch (type) {
+  //   case 'main':
+  //     if (metadata.mainCoverUrl) { return metadata.mainCoverUrl }
+  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=main` }
 
-    case 'sam':
-      if (metadata.samCoverUrl) { return metadata.samCoverUrl }
-      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=sam` }
+  //   case 'sam':
+  //     if (metadata.samCoverUrl) { return metadata.samCoverUrl }
+  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=sam` }
 
-    case 'thumb':
-      if (metadata.thumbnailCoverUrl) { return metadata.thumbnailCoverUrl }
-      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=240x240` }
+  //   case 'thumb':
+  //     if (metadata.thumbnailCoverUrl) { return metadata.thumbnailCoverUrl }
+  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=240x240` }
 
-    default:
-      // /api/cover/RJ123456.jpg?type=sam
-      return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=${type}`
-  }
+  //   default:
+  //     // /api/cover/RJ123456.jpg?type=sam
+  //     return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=${type}`
+  // }
+
+  return process.env.API_URL + `/api/cover/${metadata.id}`
 };
 
 /**
