2373a350af18da76debe6d97d13e95bdab0dab62
kahosan <15116355657l@gmail.com>
2022-09-08 21:39:50 +0800

    fix: cover url

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -13,28 +13,26 @@ export const coverURL = (metadata, type = 'main') => {
 
   // 应对组件初始化时没有提供 workId 的情况
   if (!(metadata.id || metadata.hash.split('/')[0])) { return '' }
-  console.log(type)
+
 
   // 优先选择后端返回的 url 地址
-  // switch (type) {
-  //   case 'main':
-  //     if (metadata.mainCoverUrl) { return metadata.mainCoverUrl }
-  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=main` }
+  switch (type) {
+    case 'main':
+      if (metadata.mainCoverUrl) { return metadata.mainCoverUrl }
+      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}` }
 
-  //   case 'sam':
-  //     if (metadata.samCoverUrl) { return metadata.samCoverUrl }
-  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=sam` }
+    case 'sam':
+      if (metadata.samCoverUrl) { return metadata.samCoverUrl }
+      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}` }
 
-  //   case 'thumb':
-  //     if (metadata.thumbnailCoverUrl) { return metadata.thumbnailCoverUrl }
-  //     else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=240x240` }
+    case 'thumb':
+      if (metadata.thumbnailCoverUrl) { return metadata.thumbnailCoverUrl }
+      else { return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}` }
 
-  //   default:
-  //     // /api/cover/RJ123456.jpg?type=sam
-  //     return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}.jpg?type=${type}`
-  // }
-
-  return process.env.API_URL + `/api/cover/${metadata.id}`
+    default:
+      // /api/cover/RJ123456.jpg?type=sam
+      return process.env.API_URL + `/api/cover/${metadata.id || metadata.hash.split('/')[0]}` 
+  }
 };
 
 /**
